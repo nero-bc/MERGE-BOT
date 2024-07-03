3bc07ba00a7ba76a0bedc0987f6bb001659774a6
Coder2355 <168324938+Coder2355@users.noreply.github.com>
2024-07-03 07:03:59 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -755,7 +755,7 @@ if __name__ == "__main__":
             app = web.AppRunner(await web_server())
             await app.setup()
             bind_address = "0.0.0.0"
-            await web.TCPSite(app, bind_address, PORT).start()
+            await web.TCPSite(app, bind_address, 8080).start()
 
 async def stop(self, *args):
             await super().stop()
