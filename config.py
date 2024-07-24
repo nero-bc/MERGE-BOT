684ac798860e9572c7b894f508b0f90c3c7c3734
nero-bc <126649764+nero-bc@users.noreply.github.com>
2024-07-24 07:16:28 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,14 +2,14 @@ import os
 
 
 class Config(object):
-    API_HASH = os.environ.get("a5dc7fec8302615f5b441ec5e238cd46")
-    BOT_TOKEN = os.environ.get("7116266807:AAFiuS4MxcubBiHRyzKEDnmYPCRiS0f3aGU")
-    TELEGRAM_API = os.environ.get("21740783")
-    OWNER = os.environ.get("6299192020")
-    OWNER_USERNAME = os.environ.get("speedwolf1")
-    PASSWORD = os.environ.get("Anime_Warrior")
-    DATABASE_URL = os.environ.get("mongodb+srv://Speedwolf1:speedwolf24689@cluster0.rgfywsf.mongodb.net/")
-    LOGCHANNEL = os.environ.get("-1002040299414")  # Add channel id as -100 + Actual ID
+    API_HASH = os.environ.get("API_HASH")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN")
+    TELEGRAM_API = os.environ.get("TELEGRAM_API")
+    OWNER = os.environ.get("OWNER")
+    OWNER_USERNAME = os.environ.get("OWNER_USERNAME")
+    PASSWORD = os.environ.get("PASSWORD")
+    DATABASE_URL = os.environ.get("DATABASE_URL")
+    LOGCHANNEL = os.environ.get("LOGCHANNEL")  # Add channel id as -100 + Actual ID
     GDRIVE_FOLDER_ID = os.environ.get("GDRIVE_FOLDER_ID", "root")
     USER_SESSION_STRING = os.environ.get("USER_SESSION_STRING", None)
     IS_PREMIUM = False
